6c869568cd3f0fd3db02d9509693d81fa0775823
marcobaye <marcobaye@4df02467-bbd4-4a76-a152-e7ce94205b78>
2020-06-22 20:40:38 +0000

    amended last change, now depends on v0.97
    git-svn-id: https://svn.code.sf.net/p/acme-crossass/code-0/trunk@258 4df02467-bbd4-4a76-a152-e7ce94205b78

--- src/alu.c
+++ src/alu.c
@@ -491,7 +491,7 @@ static void parse_binary_literal(void)	// Now GotByte = "%" or "b"
 	}
 	if (!digits)
 		Throw_warning("Binary literal without any digits.");	// FIXME - make into error!
-	if (digits & config.warn_bin_mask)
+	if ((digits & config.warn_bin_mask) && (config.wanted_version >= VER_BACKSLASHESCAPING))
 		Throw_first_pass_warning("Binary literal with strange number of digits.");
 	// set force bits
 	if (config.honor_leading_zeroes) {
